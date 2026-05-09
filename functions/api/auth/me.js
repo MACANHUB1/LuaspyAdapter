3d9132d75d9a5c221e1dd376f511ee546f3de35c
MACANHUB1 <dudich.kirill.v@gmail.com>
2026-05-09 14:43:25 +0300

    Update me.js

--- functions/api/auth/me.js
+++ functions/api/auth/me.js
@@ -1,1 +1,7 @@
+import { getUser, json } from "../../_lib.js";
 
+export async function onRequestGet(ctx) {
+  const user = await getUser(ctx);
+  if (!user) return json({ user: null }, 401);
+  return json({ user });
+}
